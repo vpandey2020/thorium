3d1a76bd38f54e6b63084f128f4ca3b19c7c4db4
Vipin <vpandey1990@gmail.com>
2022-02-26 19:30:37 +0530

    changes made

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // QUERY PARAMS
 // localhost:3000/get-query-1?myCoolVar=24&xyz=hiFunctionUP
-router.get("/get-query-1", function (req, res) {
+router.get("/get-query-1", function(req, res) {
     let data = req.query
     console.log(data)
     res.send({ data: data, status: true })
@@ -11,36 +11,36 @@ router.get("/get-query-1", function (req, res) {
 
 
 // take marks in req.query variable named "marks" and send "pass" if > 40 else "fail"
-router.get("/get-query-2", function (req, res) {
+router.get("/get-query-2", function(req, res) {
     let marks = req.query.marks
-    // { marks: '80'}
+        // { marks: '80'}
 
     let result = marks > 40 ? "pass" : "fail"
-    // let result = "fail"
-    // if (marks> 40) { result = "pass" }
-    // // else { result = "fail" }
+        // let result = "fail"
+        // if (marks> 40) { result = "pass" }
+        // // else { result = "fail" }
 
     res.send({ result: result, status: true })
 })
 
 //query params are also available in post request
-router.post("/post-query-1", function (req, res) {
+router.post("/post-query-1", function(req, res) {
     let data = req.query
     console.log(data)
     res.send({ result: data, status: true })
 })
 
 let myArr = [23, 45, 67, 281394, 32424, 423, 24, 42323, 4, 234, 12, 34]
-//filter out all the numbers that are greater than input( input is received from query params)
-router.post("/post-query-2", function (req, res) {
+    //filter out all the numbers that are greater than input( input is received from query params)
+router.post("/post-query-2", function(req, res) {
     //CODE HERE
-    let input= req.query.input
-    let finalArr= myArr.filter( ele => ele > input)
-    // let finalArr=[]
-    // for (i=0 ; i<myArr.length; i++) {
-    //     if ( myArr[i] > input )      finalArr.push(myArr[i]) 
-    // }
-    res.send({ result: finalArr , status: true })
+    let input = req.query.input
+    let finalArr = myArr.filter(ele => ele > input)
+        // let finalArr=[]
+        // for (i=0 ; i<myArr.length; i++) {
+        //     if ( myArr[i] > input )      finalArr.push(myArr[i]) 
+        // }
+    res.send({ result: finalArr, status: true })
 })
 
 
@@ -80,6 +80,6 @@ router.post("/post-query-2", function (req, res) {
 
 
 
-
+console.log("this is to check out whether push is working or not")
 
 module.exports = router;
