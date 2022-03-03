48ea337d5c7ec6eccf270cacd1dc257a5539336c
Vipin <vpandey1990@gmail.com>
2022-03-03 08:56:18 +0530

    changes done assignment done

--- src/controllers/publisher.js
+++ src/controllers/publisher.js
@@ -4,6 +4,7 @@ const createPublisher = async function(req, res) {
     let publisher = req.body
     let publisherCreated = await publisherModel.create(publisher)
     res.send({ data: publisherCreated })
+    console.log("assignment done")
 }
 
 module.exports.createPublisher = createPublisher
