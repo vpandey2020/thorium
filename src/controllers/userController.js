0c82c3eb8f10b352075a87d8edd3d60ed49357c8
Vipin <vpandey1990@gmail.com>
2022-03-11 07:39:05 +0530

    assignment completed and pushed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,95 +1,86 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
-const createUser = async function (req, res) {
-  try {
-    let data = req.body;
-    if (Object.keys(data).length > 0) {
-      let user = await userModel.create(req.body);
-      res.status(201).send({ status: true, msg: user });
-    } else {
-      res.status(400).send({ error: "Please provide input data" });
-    }
-  } catch (error) {
+const createUser = async function(req, res) {
+    try {
+        let data = req.body;
+        if (Object.keys(data).length > 0) {
+            let user = await userModel.create(req.body);
+            res.status(201).send({ status: true, msg: user });
+        } else {
+            res.status(400).send({ error: "details are not matching please enter correct data" });
+        }
+    } catch (error) {
         res.status(500).send({ error: error.message });
-  }
-};
-
-const userLogin = async function (req, res) {
-  try {
-    let userName = req.body.emailId;
-    let password = req.body.password;
-
-    if (userName && password) {
-      let user = await userModel.findOne({
-        emailId: userName,
-        password: password,
-      });
-      if (!user)
-        return res
-          .status(404)
-          .send({ status: false, msg: "username or password is incorrect " });
-
-      let userToken = jwt.sign({ userId: user._id.toString() }, "SurajDubey");
-
-      res.status(200).send({ status: true, msg: userToken });
-    } else {
-      res.status(400).send({ status: false, error: "Please provide user inputs" });
     }
-  } catch (error) {
-    res.status(500).send({ error: error.message });
-  }
 };
 
-const getUser = async function (req, res) {
-  try{
+const userLogin = async function(req, res) {
+    try {
+        let userName = req.body.emailId;
+        let password = req.body.password;
 
-    let id = req.params.userId;
-    let userById = await userModel.findById(id);
-    
-      res.status(200).send({ status: true, msg: userById });
-    
-  }catch (error) {
-    res.status(500).send({error: error.message})
-  }
-};
+        if (userName && password) {
+            let user = await userModel.findOne({
+                emailId: userName,
+                password: password,
+            });
+            if (!user)
+                return res
+                    .status(404)
+                    .send({ status: false, msg: "page not found or unable to access the request  " });
 
-const updateUserData = async function (req, res) {
-  try{
-  let id = req.params.userId;
-  let data = req.body;
-  if(Object.keys(data).length > 0){
+            let userToken = jwt.sign({ userId: user._id.toString() }, "VipinPandey");
 
-    let updateUser = await userModel.findByIdAndUpdate(
-      { _id: id },
-      { $set: data },
-      { new: true }
-    );
-    res.status(200).send({ status: true, msg: updateUser });
-  }else{
-    res.status(400).send({status : false, msg: "provide input data"})
-  }
-  }
-  catch (error) {
-    res.status(500).send({error : error.message})
-  }
-};
-
-const deleteUserData = async function (req, res) {
-  try {
-
-    let id = req.params.userId;
-    let deleteUser = await userModel.findByIdAndUpdate(
-      { _id: id },
-      { $set: { isDeleted: true } },
-      { new: true }
-      );
-      res.status(201).send({ status: true, msg: deleteUser })
-    }catch (error){
-        res.status(500).send({error :error.message})
+            res.status(200).send({ status: true, msg: userToken });
+        } else {
+            res.status(400).send({ status: false, error: "Please user enter data" });
+        }
+    } catch (error) {
+        res.status(500).send({ error: error.message });
     }
-  }
-    
+};
+
+const getUser = async function(req, res) {
+    try {
+
+        let id = req.params.userId;
+        let userById = await userModel.findById(id);
+
+        res.status(200).send({ status: true, msg: userById });
+
+    } catch (error) {
+        res.status(500).send({ error: error.message })
+    }
+};
+
+const updateUserData = async function(req, res) {
+    try {
+        let id = req.params.userId;
+        let data = req.body;
+        if (Object.keys(data).length > 0) {
+
+            let updateUser = await userModel.findByIdAndUpdate({ _id: id }, { $set: data }, { new: true });
+            res.status(200).send({ status: true, msg: updateUser });
+        } else {
+            res.status(400).send({ status: false, msg: "enter data" })
+        }
+    } catch (error) {
+        res.status(500).send({ error: error.message })
+    }
+};
+
+const deleteUserData = async function(req, res) {
+    try {
+
+        let id = req.params.userId;
+        let deleteUser = await userModel.findByIdAndUpdate({ _id: id }, { $set: { isDeleted: true } }, { new: true });
+        res.status(201).send({ status: true, msg: deleteUser })
+    } catch (error) {
+        res.status(500).send({ error: error.message })
+    }
+}
+
 
 module.exports.createUser = createUser;
 module.exports.userLogin = userLogin;
--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://surajxxx:MetEma4V1FgQslG7@cluster0.ik33b.mongodb.net/surajxxxx-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+mongoose.connect("mongodb+srv://vipinpandey199:MetEma4V1FgQslG7@cluster0.ik33b.mongodb.net/vipinpandey199-DB?retryWrites=true&w=majority", {
+        useNewUrlParser: true
+    })
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
 app.use('/', route)
 
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
